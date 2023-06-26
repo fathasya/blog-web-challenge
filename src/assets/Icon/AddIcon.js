0c6816b567cfe48eb126ed7ce64fbbd116543b48
FATHASYA MEYRA ARLIANSYAH <fathasyaarliansyah@gmail.com>
2023-06-27 02:29:17 +0700

    test icon

--- src/assets/Icon/AddIcon.js
+++ src/assets/Icon/AddIcon.js
@@ -1,6 +1,6 @@
 const AddIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="white" className="w-4 h-4 lg:w-6 lg:h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="3" stroke="white" className="w-5 h-5">
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
   )
